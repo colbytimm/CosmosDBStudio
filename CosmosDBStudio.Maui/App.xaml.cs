ac91bcef4f4ef743349815b3536d8acd79230683
Colby Timm <timmcolby@gmail.com>
2022-09-23 15:29:42 -0700

    Init Maui App

---
+++ CosmosDBStudio.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CosmosDBStudio.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
